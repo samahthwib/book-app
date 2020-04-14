6a9bb18a35eaa511da0a5a11488fc512a10fbdfe
samahthwib <samah.thweib@hotmail.com>
2020-04-14 04:45:58 +0300

    without styling

---
+++ books.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    authors VARCHAR (255),
+    title  VARCHAR (255),
+    isbn   VARCHAR (255),
+    smallThumbnail VARCHAR (255),
+    description text,
+    bookshelf VARCHAR (255)
+);
+
+
+
+INSERT INTO books (authors, title, isbn, smallThumbnail, description, bookshelf)
+VALUES ('samah' , 'food' , 'Not Found' , 'Not Found' , 'nothing' , 'nothing');
+
+INSERT INTO books (authors, title, isbn, smallThumbnail, description, bookshelf)
+VALUES ('saja' , 'birds' , 'Not Found' , 'Not Found' , 'nothing' , 'nothing');
